e62149764fe63cdc086efaf01afdfcd06c48e4ca
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-02-23 15:10:22 -0500

    Use RecyclerView to display sound names instead of Buttons
    activity_main.xml:
    - Remove old LinearLayout wrapped in ScrollView.
    - Add RecyclerView.
    
    drawable:
    - Add icon drawable for a play button.
    - Add drawable for a custom scrollbar thumb.
    
    list_item.xml:
    - Define layout for a single grid item view of the RecyclerView. Has an ImageButton to play the sound and a TextView to provide a description of the sound.
    
    MainActivity.java:
    - Define inner class SoundHolder. It is a RecyclerView ViewHolder implementation for displaying a sound. Also, setup ImageButton in this holder to play a sound via a View.OnClickListener setup here.
    - Define inner class SoundAdapter. It is a RecyclerView adapter implementation for adapting a List of String sound names.
    - Define onResume() to setup the RecyclerView to display sounds.

--- app/src/main/java/com/davidread/vinesoundboard/MainActivity.java
+++ app/src/main/java/com/davidread/vinesoundboard/MainActivity.java
@@ -1,20 +1,25 @@
 package com.davidread.vinesoundboard;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.List;
 
 /**
- * {@link MainActivity} is a soundboard user interface. It has a {@link Button} for each sound
- * provided by {@link Soundboard}. Clicking a button invokes {@link Soundboard#playSound(int)} to
- * play its corresponding sound.
+ * {@link MainActivity} is a soundboard user interface. It retrieves available sounds from
+ * {@link #soundboard} and displays them in {@link #soundRecyclerView}.
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     /**
      * {@link Soundboard} for getting sound names and playing sounds.
@@ -22,39 +27,155 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Soundboard soundboard;
 
     /**
-     * Invoked when this {@link MainActivity} is initially created. It initializes
-     * {@link #soundboard} and adds a {@link Button} to the activity for each sound name provided
-     * by {@link #soundboard}.
+     * {@link RecyclerView} for displaying an item view for each sound name.
+     */
+    private RecyclerView soundRecyclerView;
+
+    /**
+     * Invoked when this {@link MainActivity} is initially starting. It simply initializes the
+     * member variables of this class.
      */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Initialize soundboard with an instance.
         soundboard = Soundboard.getInstance(this);
+        soundRecyclerView = findViewById(R.id.sound_recycler_view);
+    }
 
-        // Add a Button to the buttonLayout for each sound name.
-        List<String> names = soundboard.getSoundNames();
-        LinearLayout buttonLayout = findViewById(R.id.button_layout);
-        for (int index = 0; index < names.size(); index++) {
-            Button button = new Button(this);
-            button.setText(names.get(index));
-            button.setTag(index);
-            button.setOnClickListener(this);
-            buttonLayout.addView(button);
+    /**
+     * Invoked directly before the UI of this {@link MainActivity} comes into the foreground. It
+     * sets up {@link #soundRecyclerView} to display the sound names available to play from
+     * {@link #soundboard}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Specify the sort order of the sounds in soundboard.
+        soundboard.setSortOrder(Soundboard.SortOrder.ALPHABETICAL_ASCENDING);
+
+        // Define the layout manager of soundRecyclerView.
+        soundRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+
+        // Get sound names and display them in soundRecyclerView.
+        List<String> soundNames = soundboard.getSoundNames();
+        SoundAdapter soundAdapter = new SoundAdapter(soundNames);
+        soundRecyclerView.setAdapter(soundAdapter);
+    }
+
+    /**
+     * {@link SoundHolder} is a model class that describes a single sound item view and metadata
+     * about its place within a {@link RecyclerView}.
+     */
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        /**
+         * {@link ImageButton} for playing the sound.
+         */
+        private ImageButton playSoundImageButton;
+
+        /**
+         * {@link TextView} to display the sound name.
+         */
+        private TextView soundNameTextView;
+
+        /**
+         * Int index of this {@link SoundHolder} in the {@link RecyclerView}.
+         */
+        private int index;
+
+        /**
+         * Constructs a new {@link SoundHolder}.
+         *
+         * @param inflater For inflating layouts.
+         * @param parent   Parent {@link ViewGroup} of the {@link RecyclerView}.
+         */
+        public SoundHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item, parent, false));
+            playSoundImageButton = itemView.findViewById(R.id.play_sound_image_button);
+            soundNameTextView = itemView.findViewById(R.id.sound_name_text_view);
+            playSoundImageButton.setOnClickListener(this);
+        }
+
+        /**
+         * Binds a sound from {@link Soundboard} to this {@link SoundHolder}.
+         *
+         * @param soundName {@link String} sound name to bind to this {@link SoundHolder}.
+         * @param index     Int index of this {@link SoundHolder} in the {@link RecyclerView}.
+         */
+        public void bind(String soundName, int index) {
+            playSoundImageButton.setContentDescription(getString(R.string.play_sound_content_description, soundName));
+            soundNameTextView.setText(soundName);
+            this.index = index;
+        }
+
+        /**
+         * Invoked when the {@link View} held by this {@link SoundHolder} is clicked. It plays this
+         * {@link SoundHolder}'s associated sound using {@link #soundboard}.
+         */
+        @Override
+        public void onClick(View view) {
+            soundboard.playSound(index);
         }
     }
 
     /**
-     * Invoked when a {@link Button} in {@link MainActivity} is clicked. It plays the sound
-     * corresponding to the clicked button.
-     *
-     * @param view {@link View} invoking this callback.
+     * {@link SoundAdapter} provides a binding from a {@link List} of {@link String} sound names to
+     * a {@link RecyclerView}.
      */
-    @Override
-    public void onClick(View view) {
-        int index = (int) view.getTag();
-        soundboard.playSound(index);
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+        /**
+         * {@link List} of {@link String} sound names to adapt.
+         */
+        private List<String> soundNamesList;
+
+        /**
+         * Constructs a new {@link SoundAdapter}.
+         *
+         * @param soundNamesList {@link List} of {@link String} sound names to adapt.
+         */
+        public SoundAdapter(@NonNull List<String> soundNamesList) {
+            this.soundNamesList = soundNamesList;
+        }
+
+        /**
+         * Callback method invoked when the {@link RecyclerView} needs a new empty
+         * {@link SoundHolder} to represent a sound name.
+         *
+         * @param parent   {@link ViewGroup} into which the new {@link View} will be added after it
+         *                 is bound to an adapter position.
+         * @param viewType The view type of the new {@link View}.
+         * @return A new {@link SoundHolder}.
+         */
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+            return new SoundHolder(layoutInflater, parent);
+        }
+
+        /**
+         * Callback method invoked when the {@link RecyclerView} needs to bind data to a
+         * {@link SoundHolder} at a certain position index.
+         *
+         * @param holder   {@link SoundHolder} to be bound.
+         * @param position The {@link SoundHolder}'s position index in the adapter.
+         */
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
+            holder.bind(soundNamesList.get(position), position);
+        }
+
+        /**
+         * Returns the total number of items this {@link SoundAdapter} is adapting.
+         *
+         * @return The total number of items this {@link SoundAdapter} is adapting.
+         */
+        @Override
+        public int getItemCount() {
+            return soundNamesList.size();
+        }
     }
 }
